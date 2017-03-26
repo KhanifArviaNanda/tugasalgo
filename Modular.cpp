9fa4d7fb1f505e920197ee8ea80ddf0142da04e8
KhanifArviaNanda <kenziiehan@gmail.com>
2017-03-26 12:09:46 +0700

    algoritma pemograman semester 2
    E-learning

---
+++ Modular.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+nama() {
+	cout<<"khanif arvia nanda";
+}
+
+NIM() {
+	cout<<"16.111.295";
+}
+
+main() {
+	cout<<"Nama saya ";
+	nama();
+	cout<<endl;
+	cout<<"NIM ";
+	NIM();
+	
+	return 0;
+}
